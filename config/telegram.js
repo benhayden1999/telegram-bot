e40a893ddbcf8677e5fcd11070f34627d8fd47e1
Ben Hayden <ben.hayden26@gmail.com>
2024-12-04 12:19:15 +0000

    basic telegraf start

--- config/telegram.js
+++
@@ -1,6 +0,0 @@
-// config/telegram.js
-require("dotenv").config(); // To load environment variables from .env file
-
-const TELEGRAM_API_URL = `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}`;
-
-module.exports = TELEGRAM_API_URL;
